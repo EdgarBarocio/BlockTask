6b03543ef1242e450a352a069dc4840939134761
Edgar Barocio <edgar.barocio@gmail.com>
2023-07-27 21:39:14 -0400

    add base cell viewmodel tests

--- Stocks/StocksTests/CellViewModelTests.swift
+++ Stocks/StocksTests/CellViewModelTests.swift
@@ -5,4 +5,17 @@
 //  Created by Edgar Barocio on 7/27/23.
 //
 
-import Foundation
+import XCTest
+@testable import Stocks
+
+final class CellViewModelTests: XCTestCase {
+    
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+    
+}
